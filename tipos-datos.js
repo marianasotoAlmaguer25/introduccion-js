e1123896e5053f852beb9b603ea9421957076ed6
Mariana Soto <[200~marianasotoAlmaguer25@users.noreply.github.com>
2026-04-18 13:07:41 -0600

    agregando tipos de datos actividad

---
+++ tipos-datos.js
@@ -0,0 +1,10 @@
+// Ejemplos de tipos de datos en JavaScript
+console.log(typeof 42);          // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666);        // number
+console.log(typeof true);        // boolean
+console.log(typeof 0);           // number
+console.log(typeof '');         // string
+console.log(typeof null);       // object (es un error histórico de JS)
+console.log(typeof undefined);  // undefined
+console.log(typeof FALSE);      // dará error si no está definida, pero si es texto: string
